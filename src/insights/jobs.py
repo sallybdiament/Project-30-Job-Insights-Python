bad02136c3654c97526e1ee1b0548a44f6977169
Sally Diament <sally.barzilai@gmail.com>
2023-02-21 19:38:51 -0300

    fixing: filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -71,6 +71,7 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
         List of jobs with provided job_type
     """
     jobs_by_type = []
-    if job_type in jobs["job_type"]:
-        jobs_by_type.append(jobs)
+    for job in jobs:
+        if job_type == job["job_type"]:
+            jobs_by_type.append(job)
     return jobs_by_type
