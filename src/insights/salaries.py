4b3005af2d13cef2bfcf0c0794df83050ab90301
Sally Diament <sally.barzilai@gmail.com>
2023-02-23 19:37:34 -0300

    fixing: corrigindo as funcoes max e min salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -20,14 +20,11 @@ def get_max_salary(path: str) -> int:
     """
     try:
         all_jobs = read(path)
-        all_max_salaries = set()
+        all_max_salaries = []
         for job in all_jobs:
-            all_max_salaries.add(job["max_salary"])
-        all_max_salaries = ' '.join(all_max_salaries).split()
-        all_max_salaries_valid = set()
-        for salary in all_max_salaries:
-            all_max_salaries_valid.add(int(salary))
-        return (max(all_max_salaries_valid))
+            if job["max_salary"] != '' and job["max_salary"] != 'invalid':
+                all_max_salaries.append(int(job["max_salary"]))
+        return max(all_max_salaries)
     except FileNotFoundError:
         raise FileNotFoundError(f"Not found file: {path}")
 
@@ -49,52 +46,30 @@ def get_min_salary(path: str) -> int:
     """
     try:
         all_jobs = read(path)
-        all_min_salaries = set()
+        all_min_salaries = []
         for job in all_jobs:
-            all_min_salaries.add(job["min_salary"])
-        all_min_salaries = ' '.join(all_min_salaries).split()
-        all_min_salaries_valid = set()
-        for salary in all_min_salaries:
-            all_min_salaries_valid.add(int(salary))
-        return (min(all_min_salaries_valid))
+            if job["min_salary"] != '' and job["min_salary"] != 'invalid':
+                all_min_salaries.append(int(job["min_salary"]))
+        return min(all_min_salaries)
     except FileNotFoundError:
         raise FileNotFoundError(f"Not found file: {path}")
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
+    print(job)
     try:
-        type(salary) == "integer"
-        for job in job:
-            job["min_salary"] < job["max_salary"]
-            type(job["min_salary"]) == "integer"
-            type(job["max_salary"]) == "integer"
-            job["min_salary"] != ""
-            job["max_salary"] != ""
-        return True
+        sal = int(salary)
+        max_salary = int(job["max_salary"])
+        min_salary = int(job["min_salary"])
+        if min_salary > max_salary:
+            print(int('a'))
     except ValueError:
         raise ValueError("Esta vaga não está com os valores corretos")
+    else:
+        if min_salary <= sal and sal <= max_salary:
+            return True
+        else:
+            return False
 
 
 def filter_by_salary_range(
@@ -116,3 +91,27 @@ def filter_by_salary_range(
         Jobs whose salary range contains `salary`
     """
     raise NotImplementedError
+
+
+#    """Checks if a given salary is in the salary range of a given job
+
+#     Parameters
+#     ----------
+#     job : dict
+#         The job with `min_salary` and `max_salary` keys
+#     salary : int
+#         The salary to check if matches with salary range of the job
+
+#     Returns
+#     -------
+#     bool
+#         True if the salary is in the salary range of the job, False otherwise
+
+#     Raises
+#     ------
+#     ValueError
+#         If `job["min_salary"]` or `job["max_salary"]` doesn't exists
+#         If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
+#         If `job["min_salary"]` is greather than `job["max_salary"]`
+#         If `salary` isn't a valid integer
+#     """
