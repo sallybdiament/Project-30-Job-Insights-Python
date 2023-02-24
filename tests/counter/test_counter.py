505282847e4be7a85eea4a1895ae7ae561f8fd4a
Sally Diament <sally.barzilai@gmail.com>
2023-02-23 22:15:03 -0300

    test: including test of count_ocurrencies

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+
+mock_text = "contar quantas vezes aparece python neste texto. Python"
+file_path = "src/fake_file_path"
 
 
 def test_counter():
-    pass
+    result = count_ocurrences("data/jobs.csv", "marketing")
+    assert result == 1259
