645192b10885321d4ea26537bf38508558e29c89
Sally Diament <sally.barzilai@gmail.com>
2023-02-23 22:29:33 -0300

    flake8: too long refactoring

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,6 +2,9 @@
 from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
+expected_result = {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
+
+
 def test_brazilian_jobs():
     result = read_brazilian_file(("tests/mocks/brazilians_jobs.csv"))
-    assert result[0] == {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
+    assert result[0] == expected_result
