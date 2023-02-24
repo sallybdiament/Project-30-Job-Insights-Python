da03d6f9ab2d3e6370a1e736f477d4c3cfbffcf1
Sally Diament <sally.barzilai@gmail.com>
2023-02-23 22:45:55 -0300

    test: trying test of sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,14 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+import pytest
+
+
+error_msg = "invalid sorting criteria: not_valid"
+jobs = [
+        {"max_salary": 10000, "min_salary": 200},
+        {"max_salary": 1500, "min_salary": 0},
+    ]
 
 
 def test_sort_by_criteria():
-    pass
+    with pytest.raises(KeyError, match=error_msg):
+        sort_by(jobs, "not_valid")
