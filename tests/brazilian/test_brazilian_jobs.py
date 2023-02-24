8c0d730b60bfe8caf591385fa24d7c7484678c2b
Sally Diament <sally.barzilai@gmail.com>
2023-02-23 22:28:16 -0300

    test: including test of read_brazilians_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+# import pytest
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file(("tests/mocks/brazilians_jobs.csv"))
+    assert result[0] == {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
